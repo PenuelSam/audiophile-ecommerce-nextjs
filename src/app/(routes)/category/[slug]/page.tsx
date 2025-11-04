21d6c785a2c62e7c815267a37af72ec3bf272a70
PenuelSam <penuelsamuel54@gmail.com>
2025-11-04 23:15:09 +0100

    feat: fixed build error

--- src/app/(routes)/category/[slug]/page.tsx
+++ src/app/(routes)/category/[slug]/page.tsx
@@ -13,8 +13,8 @@ function formatTitle(slug: string) {
   return slug.replace(/-/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase());
 }
 
-export default function CategoryPage({ params }: CategoryPageProps) {
-  const products = getProductsByCategory(params.slug);
+export default  function CategoryPage({ params }: { params: { slug: string } }) {
+  const products =  getProductsByCategory(params.slug);
 
   if (!products.length) {
     notFound();
@@ -24,7 +24,9 @@ export default function CategoryPage({ params }: CategoryPageProps) {
     <div className="pb-24">
       <div className="bg-black py-16 text-center text-white">
         <div className="container-width">
-          <h1 className="text-[40px] leading-[44px]  font-bold uppercase tracking-[1.43px]">{formatTitle(params.slug)}</h1>
+          <h1 className="text-[40px] leading-[44px]  font-bold uppercase tracking-[1.43px]">
+            {formatTitle(params.slug)}
+          </h1>
         </div>
       </div>
       <section className="container-width mt-20 space-y-24">
@@ -37,3 +39,4 @@ export default function CategoryPage({ params }: CategoryPageProps) {
     </div>
   );
 }
+
